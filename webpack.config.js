7673c08bd9c4515c19b9de9345f4523632160eca
ethan-yami <345548528@qq.com>
2018-08-16 18:46:45 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ module.exports = {
 	mode: 'development',
 	entry: {
 		main: './src/script/main.js',
-		a: './src/script/a.js'
+		a: './src/script/a.js',
+		b: './src/script/b.js',
+		c: './src/script/c.js'
 	},
 	output: {
 
@@ -15,17 +17,75 @@ module.exports = {
 	},
 	plugins:[
 		new htmlWebpackPlugin({
-			filename:"index-[hash].html",
+			filename:"index.html",
 			template:"index.html",
+			inject:'body',			
 			minify:{
 				removeComments:true,
 				removeEmptyElements:true,
-				removeTagWhitespace:true,
-				collapseWhitespace:true,
+				/*removeTagWhitespace:true,
+				collapseWhitespace:true,*/
 			},
-			inject:true,
+			
 			title:'webpack is pretty good',
-			date:new Date()
+			date:new Date(),
+			chunks:['main','a']
+		}),
+		new htmlWebpackPlugin({
+			filename:"login.html",
+			template:'login.html',
+			inject:'body',
+			
+			minify:{
+				removeComments:true,
+				removeEmptyElements:true,
+			/*	removeTagWhitespace:true,
+				collapseWhitespace:true,*/
+			},
+			chunks:['main']
+		}),
+		new htmlWebpackPlugin({
+			filename:"a.html",
+			template:'a.html',
+			inject:'body',		
+			minify:{
+				removeComments:true,
+				removeEmptyElements:true,
+				/*removeTagWhitespace:true,
+				collapseWhitespace:true,*/
+			},
+			chunks:['a']
+
+		}),
+
+		new htmlWebpackPlugin({
+			filename:"b.html",
+			template:'b.html',
+			inject:'body',
+			minify:{
+				removeComments:true,
+				removeEmptyElements:true,
+				/*removeTagWhitespace:true,
+				collapseWhitespace:true,*/
+			},
+			chunks:['b']
+
+
+		}),
+
+		new htmlWebpackPlugin({
+			filename:"c.html",
+			template:'c.html',
+			inject:"body",
+			minify:{
+				removeComments:true,
+				removeEmptyElements:true,
+				/*removeTagWhitespace:true,
+				collapseWhitespace:true,*/
+			},
+			chunks:['c']
+
+
 		})
 	]
 
